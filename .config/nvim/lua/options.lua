66192b01c917358368051d6b131954dad0f1ba6d
Rishab Garg <me@rishab-garg.me>
2025-01-21 16:17:59 +0100

    feat: re-add nvim config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,69 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+vim.opt.showmode = false
+
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.opt.breakindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+
+vim.opt.guicursor =
+	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
+vim.schedule(function()
+	vim.api.nvim_set_hl(0, "Cursor", { fg = "#ffffff", bg = "#528bff" })
+end)
+
+-- [[ keymaps ]]
+vim.keymap.set("n", "<leader>m", "<cmd>Mason<cr>", { desc = "mason" })
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "lazy" })
+vim.keymap.set("n", "<Left>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
+vim.keymap.set("n", "<Up>", "<Nop>")
+-- vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "exit terminal mode" }) -- exit terminal mode
+
+-- [[ autocommands ]]
+vim.api.nvim_create_autocmd("TextYankPost", { -- highlight when yanking (copying) text
+	desc = "highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
